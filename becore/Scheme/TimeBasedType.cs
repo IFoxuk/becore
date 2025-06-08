f9bd9f359f6581e631018d1f05d8fda19b622460
fxk <foxxxuk@vk.com>
2025-06-08 13:44:44 +0500

    TBT created

---
+++ becore/Scheme/TimeBasedType.cs
@@ -0,0 +1,18 @@
+﻿namespace becore.Scheme;
+
+public abstract class TimeBasedType<T>
+{
+    public Guid Id { get; set; }
+    public T Value { get; set; }
+    public Guid ParentId { get; set; }
+    public TimeBasedType<T> Parent { get; set; }
+    public Guid ChildId { get; set; }
+    public TimeBasedType<T> Child { get; set; }
+    public DateTime CreatedAt { get; } = DateTime.Now;
+}
+
+public class TimeBasedString : TimeBasedType<string> { }
+public class TimeBasedInt : TimeBasedType<int> { }
+public class TimeBasedBool : TimeBasedType<bool> { }
+public class TimeBasedByte : TimeBasedType<byte> { }
+public class TimeBasedDouble : TimeBasedType<double> { }
