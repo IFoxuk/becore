78a5998e5fba57de4128d5a13fd14f970a8643e1
fxk <foxxxuk@vk.com>
2025-06-24 23:34:44 +0500

    Добавлен контент для страницы

--- becore.api/Scheme/Page.cs
+++ becore.api/Scheme/Page.cs
@@ -9,6 +9,7 @@ public class Page : DbEntity
 {
     [Required] [MaxLength(32)] public required string Name { get; set; }
     [MaxLength(256)] public string? Description { get; set; }
+    [MaxLength(2048)] public string? Content { get; set; }
     public string? QuadIcon { get; set; }
     public string? WideIcon { get; set; }
     [NotMapped] public List<Pack> Packs { get; set; } = [];
